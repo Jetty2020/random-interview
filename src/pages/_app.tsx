359276b3a80e4f190610bcd2a10ed65da87d1c0c
JeongHoon Park <kmclo@naver.com>
2022-03-28 00:18:40 +0900

    :lipstick: 글로벌 CSS 및 Reset CSS 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
+import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import { GlobalStyles } from '../styles/globals';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={GlobalStyles} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;

d22b5f956aae13d9a1abc16ba7dba951e951a1d6
Tesseract <tensai3355@gmail.com>
2022-03-31 11:15:37 +0900

    :truck: 코드 컨벤션에 맞게 컴포넌트 변수명 변경

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from 'next';
 import PageTitle from '@components/common/PageTitle';
-import CategoryList from '@components/questionList/CategoryList';
+import ListCategory from '@components/questionList/ListCategory';
 import { CATEGORIES } from '@constants/categories';
 import { useCallback, useState } from 'react';
-import QuestionList from '@components/questionList/QuestionList';
-import QuestionContainer from '@components/questionList/QuestionContainer';
+import ListQuestion from '@components/questionList/ListQuestion';
+import WrapperQuestion from '@components/questionList/WrapperQuestion';
 
 const QuestionListPage: NextPage = () => {
   const [selection, setSelection] = useState<boolean[]>(
@@ -23,10 +23,10 @@ const QuestionListPage: NextPage = () => {
   return (
     <>
       <PageTitle title="면접 질문 목록" />
-      <QuestionContainer>
-        <CategoryList selection={selection} categoryClick={categoryClick} />
-        <QuestionList selection={selection} />
-      </QuestionContainer>
+      <WrapperQuestion>
+        <ListCategory selection={selection} categoryClick={categoryClick} />
+        <ListQuestion selection={selection} />
+      </WrapperQuestion>
     </>
   );
 };

3405f7037256344305ea506f5cd014f903eff82a
정민킴 <jma1020@naver.com>
2022-04-06 08:40:34 +0900

    💄 임시 푸터 컴포넌트 생성

---
+++ src/components/layouts/FooterPage.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+function FooterPage() {
+  return <Footer>푸터</Footer>;
+}
+
+const Footer = styled.footer`
+  height: 60px;
+  background-color: green;
+`;
+
+export default FooterPage;

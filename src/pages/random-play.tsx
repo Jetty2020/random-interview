6cf12ff9920c9321860824c1fe2e4ecee3863f75
Chun-gu <chungu.yi92@gmail.com>
2022-04-05 19:45:03 +0900

    ✨ random-play 페이지 추가

---
+++ src/pages/random-play.tsx
@@ -0,0 +1,59 @@
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Media from '@components/Media';
+import Link from 'next/link';
+
+interface IMediaProps {
+  recordMethod: string | undefined;
+  audioInput: string | undefined;
+  videoInput: string | undefined;
+  isRecording: boolean;
+}
+
+const RandomPlay: NextPage = () => {
+  const router = useRouter();
+  const [downloadLink, setDownloadLink] = useState<string>('');
+  const { recordMethod, audioInput, videoInput } =
+    router.query as unknown as IMediaProps;
+
+  const goToResult = (e: { preventDefault: () => void }) => {
+    console.log('result 페이지로 보내는', downloadLink);
+    e.preventDefault();
+    router.push({
+      pathname: '/result',
+      // query: { downloadLink },
+    });
+  };
+
+  useEffect(() => {
+    console.log('RandomPlay 페이지 마운트');
+
+    return () => {
+      console.log('RandomPlay 페이지 언마운트');
+    };
+  }, [downloadLink]);
+
+  return (
+    <>
+      {(recordMethod === 'audio' ||
+        recordMethod === 'video' ||
+        recordMethod === 'full') && (
+        <Media
+          isTest={false}
+          isRecording={true}
+          recordMethod={recordMethod}
+          audioInput={audioInput}
+          videoInput={videoInput}
+          setDownloadLink={setDownloadLink}
+        />
+      )}
+      {/* <a href="/result" onClick={goToResult}>
+        결과 페이지로 이동
+      </a> */}
+      <Link href="/result">결과 페이지로 이동 링크</Link>
+    </>
+  );
+};
+
+export default RandomPlay;

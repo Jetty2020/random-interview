776e5d252d76a7a0fca8adfcb4aa456399d0e651
Chun-gu <chungu.yi92@gmail.com>
2022-04-05 19:43:05 +0900

    ✨ Media 컴포넌트 추가

---
+++ src/components/Media/styled.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const VideoContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Video = styled.video`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Guideline = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;

ee6afc5cedaa708f507e5f66aa024ee54d28c0d1
정민킴 <jma1020@naver.com>
2022-04-06 08:52:08 +0900

    🔥 예시 코드들 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
 import type { NextPage } from 'next';
-import Example from '@components/landing/Example';
 import PageTitle from '@components/common/PageTitle';
 
 const Home: NextPage = () => {
   return (
     <div>
       <PageTitle title="홈" />
-      <h1>H1</h1>
-      <div>Home</div>
-      <Example />
     </div>
   );
 };

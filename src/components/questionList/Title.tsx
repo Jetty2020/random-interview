993c9a94ad0e11a548dc6f2024fdb5b19408697e
Tesseract <tensai3355@gmail.com>
2022-04-06 16:59:16 +0900

    :lipstick: 페이지 제목 및 설명 추가

---
+++ src/components/questionList/Title.tsx
@@ -0,0 +1,29 @@
+import { GRAY_500 } from '@constants/colors';
+import styled from '@emotion/styled';
+import { pxToRem } from '@utils/pxToRem';
+
+const TitleQuestionListPage = () => {
+  return (
+    <Container>
+      <Title>면접 질문 목록</Title>
+      <Desc>면접 질문 목록을 카테고리별 열람 및 검색할 수 있습니다.</Desc>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  height: ${pxToRem(100)};
+  padding: ${pxToRem(50)} 0 0 ${pxToRem(30)};
+`;
+
+const Title = styled.h2`
+  margin-bottom: ${pxToRem(10)};
+  font-size: ${pxToRem(36)};
+`;
+
+const Desc = styled.p`
+  font-size: ${pxToRem(16)};
+  color: ${GRAY_500};
+`;
+
+export default TitleQuestionListPage;
--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next';
 import { useCallback, useState } from 'react';
+import { CATEGORIES } from '@constants/categories';
 import ContainerPage from '@components/common/ContainerPage';
 import PageTitle from '@components/common/PageTitle';
-import ListCategory from '@components/questionList/ListCategory';
-import { CATEGORIES } from '@constants/categories';
-import ListQuestion from '@components/questionList/ListQuestion';
+import TitleQuestionListPage from '@components/questionList/Title';
 import WrapperQuestion from '@components/questionList/WrapperQuestion';
+import ListCategory from '@components/questionList/ListCategory';
+import ListQuestion from '@components/questionList/ListQuestion';
 
 const QuestionListPage: NextPage = () => {
   const [selection, setSelection] = useState<boolean[]>(
@@ -24,6 +25,7 @@ const QuestionListPage: NextPage = () => {
   return (
     <ContainerPage>
       <PageTitle title="면접 질문 목록" />
+      <TitleQuestionListPage />
       <WrapperQuestion>
         <ListCategory selection={selection} categoryClick={categoryClick} />
         <ListQuestion selection={selection} />

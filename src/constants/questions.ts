81839963f10203b44f0ad808a83e39aa3e50b95c
Tesseract <tensai3355@gmail.com>
2022-04-08 03:07:40 +0900

    :speech_balloon: UI 확인을 위해 QUESTIONS 변경

--- src/constants/questions.ts
+++ src/constants/questions.ts
@@ -1,88 +1,16 @@
+import { CATEGORIES } from './categories';
+
 export interface QuestionData {
   question: string;
   answer: string;
   category: string;
 }
 
-export const QUESTIONS = [
-  {
-    question: '질문1',
-    answer: '답변1',
-    category: 'CS지식',
-  },
-  {
-    question: 'HTML 질문2',
-    answer: 'HTML 답변2',
-    category: 'HTML',
-  },
-  {
-    question: 'CSS 질문3',
-    answer: 'CSS답변3',
-    category: 'CSS',
-  },
-  {
-    question: 'JS 질문4',
-    answer: 'JS 답변4',
-    category: 'JS',
-  },
-  {
-    question: 'React 질문5',
-    answer: 'React 답변5',
-    category: 'React',
-  },
-  {
-    question: 'Web 질문6',
-    answer: 'Web 답변6',
-    category: 'Web',
-  },
-  {
-    question: '질문7',
-    answer: '답변7',
-    category: '프로젝트',
-  },
-  {
-    question: '질문8',
-    answer: '답변8',
-    category: '기타',
-  },
-  {
-    question: 'React 질문9',
-    answer: 'React 답변9',
-    category: 'React',
-  },
-  {
-    question: 'Web 질문10',
-    answer: 'Web 답변10',
-    category: 'Web',
-  },
-  {
-    question: 'CSS 질문11',
-    answer: 'CSS 답변11',
-    category: 'CSS',
-  },
-  {
-    question: 'HTML 질문12',
-    answer: 'HTML 답변12',
-    category: 'HTML',
-  },
-  {
-    question: 'React 질문13',
-    answer: 'React 답변13',
-    category: 'React',
-  },
-  {
-    question: 'JS 질문14',
-    answer: 'JS 답변14',
-    category: 'JS',
-  },
-  {
-    question: 'Web 질문15',
-    answer: 'Web 답변15',
-    category: 'Web',
-  },
-  {
-    question: 'Web 질문16',
-    answer: 'Web 답변16',
-    category: 'Web',
-  },
-];
+export const QUESTIONS = [...Array(234)].map((_, i) => {
+  const rand = Math.floor(Math.random() * CATEGORIES.length);
+  return {
+    question: `${CATEGORIES[rand]} 질문${i + 1}`,
+    answer: `${CATEGORIES[rand]} 답변${i + 1}`,
+    category: CATEGORIES[rand],
+  };
+});

e9ae52e97ebfda34238fdd7e7b24acc65c1df1a2
JeongHoon Park <kmclo@naver.com>
2022-04-08 17:43:31 +0900

    :sparkles: 랜덤 문제 정보 받아오기 기능 구현

--- src/components/ramdom-interview/QuestionList.tsx
+++ src/components/ramdom-interview/QuestionList.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { QUESTIONS } from '@constants/questions';
 import { CATEGORIES } from '@constants/categories';
 
@@ -7,5 +6,17 @@ interface StartInterviewProps {
 }
 
 export const QuestionList = ({ questionIndexArr }: StartInterviewProps) => {
+  const questionArr = Array(0);
+  CATEGORIES.forEach((_) => questionArr.push([]));
+
+  for (let i = 0; i < questionIndexArr.length; i++) {
+    const questionCategoryArr = QUESTIONS.filter(
+      (ele) => ele.category === CATEGORIES[i],
+    );
+    for (let j = 0; j < questionIndexArr[i].length; j++) {
+      questionArr[i].push(questionCategoryArr[questionIndexArr[i][j]]);
+    }
+  }
+  console.log(questionArr[0]);
   return <>ddu</>;
 };

da3cc0448cbbadfa894f241c0c55f32f505efb6e
skgml0 <hnh2714@gmail.com>
2022-04-09 17:06:55 +0900

    💄 Quiz UI 구현 완료

--- src/components/ramdom-interview/Quiz.tsx
+++ src/components/ramdom-interview/Quiz.tsx
@@ -1,15 +1,29 @@
+import styled from '@emotion/styled';
+import {
+  QuestionMark,
+  Text,
+  AnswerMark,
+} from '@components/questionList/Question';
+import { pxToRem } from '@utils/pxToRem';
+
 interface quizProps {
   question: string;
   answer: string;
-  category: string;
 }
-export const Quiz = ({ question, answer, category }: quizProps) => {
-  console.log(question, answer, category);
+export const Quiz = ({ question, answer }: quizProps) => {
+  console.log(question, answer);
   return (
-    <div>
-      <p>{category}</p>
-      <p>{question}</p>
-      <p>{answer}</p>
-    </div>
+    <WrapQuize>
+      {/* <CategoryMark>{category}</CategoryMark> */}
+      <QuestionMark>Q.</QuestionMark>
+      <Text>{question}</Text>
+      <br />
+      <AnswerMark>A.</AnswerMark>
+      <Text>{answer}</Text>
+    </WrapQuize>
   );
 };
+
+const WrapQuize = styled.div`
+  margin: ${pxToRem(50)} ${pxToRem(40)};
+`;
